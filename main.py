f5bccffa629bb0936d2ce541cfde3b7b8490698d
yeop0740 <ssucgoo@gmail.com>
2023-09-12 20:21:45 +0900

    feat: 서울시 병원정보 csv 파싱 후 데이터베이스 insert

---
+++ main.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+from sqlalchemy import create_engine
+
+## 서울시 병의원 위치 정보, https://data.seoul.go.kr/dataList/OA-20337/S/1/datasetView.do
+hospitals = pd.read_csv("/Users/heeyeop/Downloads/서울시 병의원 위치 정보.csv", encoding="cp949")
+
+data = hospitals[['주소', '비고', '기관설명상세', '기관명', '대표전화1', '병원경도', '병원위도']]
+
+a = data.loc[
+    (data['비고'].str.contains("정신", na=False)) |
+    (data['기관설명상세'].str.contains("정신", na=False)) |
+    (data['기관명'].str.contains('정신', na=False))
+    ]
+
+a['주소'] = a['주소'].astype('str')
+splited_address = a['주소'].str.split(',')
+a['area'] = splited_address.str.get(1)
+a['주소a'] = splited_address.str.get(0)
+a['주소a'] = a['주소a'].astype('str')
+areas = a['주소a'].str.split(' ')
+a['area1'] = areas.str.get(0)
+a['area2'] = areas.str.get(1)
+a['area3'] = areas.str.get(2).str.cat(areas.str.get(3), sep=' ')
+
+final = a[['기관명', '대표전화1', 'area1', 'area2', 'area3', 'area']]
+
+# areas = splited_address[0].astype('str').split(' ')
+# areas = splited_address.str.split(' ')
+
+toSQL = final.rename(columns={'기관명':'name', '대표전화1':'telephone'})
+toSQL['create_date'] = ''
+toSQL['last_modified_date'] = ''
+toSQL['average_rating'] = ''
+toSQL['number_of_reviews'] = ''
+toSQL['total_rating'] = ''
+toSQL['url'] = ''
+toSQL['map_url'] = ''
+
+engine = create_engine('', echo=True)
+toSQL.to_sql(name='hospital', if_exists='append' ,con=engine, index=False)
