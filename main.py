8f580faee6eb5eecebc0b4c10e51d68fd6d0be8b
yeop0740 <ssucgoo@gmail.com>
2023-09-17 00:33:16 +0900

    fix: main.py yml 설정 추가

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import pandas as pd
+import yaml as yml
 
 from sqlalchemy import create_engine
 
-## 서울시 병의원 위치 정보, https://data.seoul.go.kr/dataList/OA-20337/S/1/datasetView.do
-hospitals = pd.read_csv("/Users/heeyeop/Downloads/서울시 병의원 위치 정보.csv", encoding="cp949")
+with open('info.yml') as info:
+    data = yml.load(info, Loader=yml.FullLoader)
+
+url = data['dataSource']['url']
+file = data['file']
+
+
+# print(data['dataSource']['url'])
+# print(file)
+
+# 서울시 병의원 위치 정보, https://data.seoul.go.kr/dataList/OA-20337/S/1/datasetView.do
+hospitals = pd.read_csv(file, encoding="cp949")
 
 data = hospitals[['주소', '비고', '기관설명상세', '기관명', '대표전화1', '병원경도', '병원위도']]
 
@@ -13,8 +24,11 @@ a = data.loc[
     (data['기관명'].str.contains('정신', na=False))
     ]
 
+for stri in a['주소']:
+    print(stri)
+
 a['주소'] = a['주소'].astype('str')
-splited_address = a['주소'].str.split(',')
+splited_address = a['주소'].str.split(',', maxsplit=1)
 a['area'] = splited_address.str.get(1)
 a['주소a'] = splited_address.str.get(0)
 a['주소a'] = a['주소a'].astype('str')
@@ -29,13 +43,14 @@ final = a[['기관명', '대표전화1', 'area1', 'area2', 'area3', 'area']]
 # areas = splited_address.str.split(' ')
 
 toSQL = final.rename(columns={'기관명':'name', '대표전화1':'telephone'})
-toSQL['create_date'] = ''
-toSQL['last_modified_date'] = ''
-toSQL['average_rating'] = ''
-toSQL['number_of_reviews'] = ''
-toSQL['total_rating'] = ''
-toSQL['url'] = ''
+# toSQL['create_date'] = ''
+# toSQL['last_modified_date'] = ''
+# toSQL['average_rating'] = ''
+# toSQL['number_of_reviews'] = ''
+# toSQL['total_rating'] = ''
+# toSQL['url'] = ''
 toSQL['map_url'] = ''
 
-engine = create_engine('', echo=True)
-toSQL.to_sql(name='hospital', if_exists='append' ,con=engine, index=False)
+
+# engine = create_engine(url, echo=True)
+# toSQL.to_sql(name='hospital', if_exists='append' ,con=engine, index=False)
